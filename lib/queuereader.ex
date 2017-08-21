b1b23c2dbb3ac2da1d6d988b72ee12c57c08a321
Elias Martenson <lokedhs@gmail.com>
2017-08-21 19:03:09 +0800

    Make the queue reader generic so that it can handle different providers

--- lib/queuereader.ex
+++ lib/queuereader.ex
@@ -1,58 +1,64 @@
 defmodule PotatoApns.QueueReader do
   use GenServer
 
+  defmodule Connection do
+    defstruct channel: "", provider: ""
+  end
+
   def start_link(opts) do
-    GenServer.start_link(__MODULE__, :ok, opts)
+    GenServer.start_link(__MODULE__, {:ok, "apns"}, opts)
   end
 
   @exchange "apns-sender-ex"
   @queue "apns-sender"
 
-  def init(:ok) do
-    rabbitmq_connect()
+  def init({:ok, provider_name}) do
+    rabbitmq_connect(provider_name)
   end
 
-  defp rabbitmq_connect do
+  defp rabbitmq_connect(provider_name) do
     case AMQP.Connection.open(host: "10.137.2.26") do
       {:ok, conn} ->
+        name = @queue <> "-" <> provider_name
+
         {:ok, channel} = AMQP.Channel.open(conn)
-        {:ok, queue} = AMQP.Queue.declare(channel, @queue, [arguments: [{"x-message-ttl", :long, 60 * 60 * 1000}]])
+        {:ok, queue} = AMQP.Queue.declare(channel, name, [arguments: [{"x-message-ttl", :long, 60 * 60 * 1000}]])
 
         queue_name = queue.queue
         IO.puts "Queue name: #{queue_name}"
 
-        :ok = AMQP.Queue.bind(channel, queue_name, @exchange, [routing_key: "#"])
+        :ok = AMQP.Queue.bind(channel, queue_name, @exchange, [routing_key: "*." <> provider_name])
         {:ok, _ctag} = AMQP.Basic.consume(channel, queue_name)
-        {:ok, channel}
+        {:ok, %Connection{channel: channel, provider: provider_name}}
       {:error, _} ->
         :timer.sleep(10000)
-        rabbitmq_connect()
+        rabbitmq_connect(provider_name)
     end
   end
 
-  def handle_info({:DOWN, _, :process, _pid, _reason}, _) do
-    {:ok, channel} = rabbitmq_connect()
-    {:noreply, channel}
+  def handle_info({:DOWN, conn, :process, _pid, _reason}, _) do
+    {:ok, conn} = rabbitmq_connect(conn.provider)
+    {:noreply, conn}
   end
 
-  def handle_info({:basic_consume_ok, %{consumer_tag: _consumer_tag}}, chan) do
+  def handle_info({:basic_consume_ok, %{consumer_tag: _consumer_tag}}, conn) do
     IO.puts "consumer registered"
-    {:noreply, chan}
+    {:noreply, conn}
   end
 
-  def handle_info({:basic_cancel, %{consumer_tag: _consumer_tag}}, chan) do
+  def handle_info({:basic_cancel, %{consumer_tag: _consumer_tag}}, conn) do
     IO.puts "consumer cancelled by error"
-    {:stop, :normal, chan}
+    {:stop, :normal, conn}
   end
 
-  def handle_info({:basic_cancel_ok, %{consumer_tag: _consumer_tag}}, chan) do
+  def handle_info({:basic_cancel_ok, %{consumer_tag: _consumer_tag}}, conn) do
     IO.puts "normal cancel"
-    {:noreply, chan}
+    {:noreply, conn}
   end
 
-  def handle_info({:basic_deliver, payload, %{delivery_tag: tag, redelivered: redelivered}}, channel) do
-    spawn fn -> consume(channel, tag, redelivered, payload) end
-    {:noreply, channel}
+  def handle_info({:basic_deliver, payload, %{delivery_tag: tag, redelivered: redelivered}}, conn) do
+    spawn fn -> consume(conn.channel, tag, redelivered, payload) end
+    {:noreply, conn}
   end
 
   defp consume(channel, tag, _redelivered, payload) do
